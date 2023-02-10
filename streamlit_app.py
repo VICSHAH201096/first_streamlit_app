502bdccc5c0960fa65f468c7f921f58e9185b36e
VICSHAH201096 <124766087+VICSHAH201096@users.noreply.github.com>
2023-02-10 14:50:22 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
-import pandas
-import requests
+# import pandas
+# import requests
 import snowflake.connector
+import urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
